cbdf095048d02c97575443e36ee690f11c840f53
Tolga Ayvazoglu <tolga.ayvazoglu@devoteam.com>
2023-12-06 10:34:30 +0100

    fix keybindings of harpooon

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -285,12 +285,12 @@ vim.keymap.set('n', '<leader>J', ":diffget //3<CR>")
 
 -- [[ Configure Harpoon ]]
 require('harpoon').setup {
-  vim.keymap.set("n", "<leader>a", require('harpoon.mark').add_file, {}),
-  vim.keymap.set("n", "<leader>e", require('harpoon.ui').toggle_quick_menu, {}),
-  vim.keymap.set("n", "<leader>h", function() require('harpoon.ui').nav_file(1) end),
-  vim.keymap.set("n", "<leader>j", function() require('harpoon.ui').nav_file(2) end),
-  vim.keymap.set("n", "<leader>l", function() require('harpoon.ui').nav_file(4) end),
-  vim.keymap.set("n", "<leader>k", function() require('harpoon.ui').nav_file(3) end),
+  vim.keymap.set("n", "<C-a>", require('harpoon.mark').add_file, {}),
+  vim.keymap.set("n", "<C-e>", require('harpoon.ui').toggle_quick_menu, {}),
+  vim.keymap.set("n", "<C-h>", function() require('harpoon.ui').nav_file(1) end),
+  vim.keymap.set("n", "<C-j>", function() require('harpoon.ui').nav_file(2) end),
+  vim.keymap.set("n", "<C-l>", function() require('harpoon.ui').nav_file(4) end),
+  vim.keymap.set("n", "<C-k>", function() require('harpoon.ui').nav_file(3) end),
 }
 -- [[ Configuration Prettier ]]
 local null_ls = require("null-ls")
